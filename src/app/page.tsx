547890fcfda9c86d3c620247c6f20cbcaaefcebf
Supratik Man Singh Pradhan <np01cp4a210087@islingtoncollege.edu.np>
2024-06-14 14:47:05 +0545

    Home page browse for category component done Signed-off-by: Supratik Man Singh Pradhan <np01cp4a210087@islingtoncollege.edu.np>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,65 +3,79 @@ import Layout from "@/layout/layout";
 
 export default function Home() {
   return (
-    
-      <div className=" p-6 overflow-y-auto h-80vh">
-        <h1 className="text-3xl font-bold mb-4">Welcome to Next.js</h1>
-        {/* Add your content here */}
-        <div>
-          <p className="text-lg">
-                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non ullamcorper lacus, eget tincidunt nunc. Donec vitae dui vel nisl condimentum ultricies.
-                          Paragraphs are a concept that people are expected to learn during their high school years but people fail to grasp the importance of paragraph length. This is not an issue that I have seen on student papers but also on academic papers submitted to journals and conferences.
+    <div className="bg-white min-h-screen">
+      <div className="p-6 overflow-y-auto h-80vh">
+        <h1 className="text-1xl md:text-3xl font-bold text-black mb-4">Browse by Category</h1>
+        <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-4 md:gap-6 lg:gap-8">
+          {/* Placeholder for the fourth item on larger screens */}
+          <div className="hidden"></div>
 
-              So what is a long paragraph? It is a paragraph that feels a bit much but if I were to summarize it, here’s two identifiers:
+          <div className="group flex sm:flex-row md:flex-col">
+            <div className="relative h-32 md:h-64 lg:h-96 w-full order-2 sm:order-1">
+              <img src="/image.png" alt="New in Fabrics" className="w-full h-full object-cover" />
+            </div>
+            {/* Different caption for small screens */}
+            <div className="bg-white bg-opacity-80 p-2 flex items-center w-full order-1 sm:order-2">
+              <p className="text-sm md:text-xs text-black hidden sm:block">NEW IN FABRICS →</p>
+              <p className="text-sm md:text-xs text-black sm:hidden">
+                <span className="block mb-1">Fabrics</span>
+                <span className="font-bold">NEW IN →</span>
+              </p>
+            </div>
+          </div>
 
-              Paragraph consisting typically of more than 8 sentences.
-              Paragraph that is not coherent.
-              These are not rigid rules to be followed but rather guidelines where you need to at least ponder on whether you paragraph is unjustly too long.
+          <div className="hidden"></div>
 
-              So what about coherence? Do not pack a bunch of content into one paragraph. For example, if you are listing a bunch 3–4 papers and their findings in a paragraph, it is acceptable to do so as long as you stay within e.g., 8 sentences. The further away that you are getting from this flexible target, the more likely it is that your paragraph is becoming too long. For example, in literature review a section make look like this:
+          <div className="group flex sm:flex-row md:flex-col">
+            <div className="relative h-32 md:h-64 lg:h-96 w-full order-2 sm:order-1">
+              <img src="/image.png" alt="New in Wallpapers" className="w-full h-full object-cover" />
+            </div>
+            {/* Different caption for small screens */}
+            <div className="bg-white bg-opacity-80 p-2 flex items-center w-full order-1 sm:order-2">
+              <p className="text-sm md:text-xs text-black hidden sm:block">NEW IN WALLPAPERS →</p>
+              <p className="text-sm md:text-xs text-black sm:hidden">
+                <span className="block mb-1">Wallpapers</span>
+                <span className="font-bold">NEW IN →</span>
+              </p>
+            </div>
+          </div>
 
+          <div className="hidden"></div>
 
-              This expansion typically is in part due to the fact that especially in literature review sections, people tend to want to mention things about the referenced papers (often more than what is required). If a single paper that you are citing is summarized in 4–5 sentences then chances are it requires its own paragraph. But then the paragraphs may look as if they are appearing out of the blue. How do you fix that? You can use an intro paragraph for your subsection like the image below.
+          <div className="group flex sm:flex-row md:flex-col">
+            <div className="relative h-32 md:h-64 lg:h-96 w-full order-2 sm:order-1">
+              <img src="/image.png" alt="New in Trims" className="w-full h-full object-cover" />
+            </div>
+            {/* Different caption for small screens */}
+            <div className="bg-white bg-opacity-80 p-2 flex items-center w-full order-1 sm:order-2">
+              <p className="text-sm md:text-xs text-black hidden sm:block">NEW IN TRIMS →</p>
+              <p className="text-sm md:text-xs text-black sm:hidden">
+                <span className="block mb-1">Trims</span>
+                <span className="font-bold">NEW IN →</span>
+              </p>
+            </div>
+          </div>
 
+          {/* Placeholder for the fourth item on larger screens */}
+          <div className="hidden"></div>
 
-              It is a handy trick that can be used and highly effective for most sections of a research paper.
-
-              Detecting long paragraphs automatically
-              That got me wondering, is there an automated way to tell if your paragraphs are too long? Two applications that I am familiar with and can do this is Grammarly and Citasion although you need to use their premium version to get this feature (free versions have some features but not related to long paragraphs). Grammarly (starting $12/month) has no trial available for testing. Citasion (starting $2.99/month) has a three month trial for its premium features.
-
-              I decided to test each on 3 texts that are intentionally too long. To simplify things, I used paragraphs from Wikipedia articles that were contiguous and just merged them into a larger paragraph.
-
-              The first article was about earthquakes:
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non ullamcorper lacus, eget tincidunt nunc. Donec vitae dui vel nisl condimentum ultricies. Paragraphs are a concept that people are expected to learn during their high school years but people fail to grasp the importance of paragraph length. This is not an issue that I have seen on student papers but also on academic papers submitted to journals and conferences. So what is a long paragraph? It is a paragraph that feels a bit much but if I were to summarize it, here’s two identifiers: Paragraph consisting typically of more than 8 sentences. Paragraph that is not coherent. These are not rigid rules to be followed but rather guidelines where you need to at least ponder on whether you paragraph is unjustly too long. So what about coherence? Do not pack a bunch of content into one paragraph. For example, if you are listing a bunch 3–4 papers and their findings in a paragraph, it is acceptable to do so as long as you stay within e.g., 8 sentences. The further away that you are getting from this flexible target, the more likely it is that your paragraph is becoming too long. For example, in literature review a section make look like this: This expansion typically is in part due to the fact that especially in literature review sections, people tend to want to mention things about the referenced papers (often more than what is required). If a single paper that you are citing is summarized in 4–5 sentences then chances are it requires its own paragraph. But then the paragraphs may look as if they are appearing out of the blue. How do you fix that? You can use an intro paragraph for your subsection like the image below. It is a handy trick that can be used and highly effective for most sections of a research paper. Detecting long paragraphs automatically That got me wondering, is there an automated way to tell if your paragraphs are too long? Two applications that I am familiar with and can do this is Grammarly and Citasion although you need to use their premium version to get this feature (free versions have some features but not related to long paragraphs). Grammarly (starting $12/month) has no trial available for testing. Citasion (starting $2.99/month) has a three month trial for its premium features. I decided to test each on 3 texts that are intentionally too long. To simplify things, I used paragraphs from Wikipedia articles that were contiguous and just merged them into a larger paragraph. The first article was about earthquakes:
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non ullamcorper lacus, eget tincidunt nunc. Donec vitae dui vel nisl condimentum ultricies.
-                          Paragraphs are a concept that people are expected to learn during their high school years but people fail to grasp the importance of paragraph length. This is not an issue that I have seen on student papers but also on academic papers submitted to journals and conferences.
-
-              So what is a long paragraph? It is a paragraph that feels a bit much but if I were to summarize it, here’s two identifiers:
-
-              Paragraph consisting typically of more than 8 sentences.
-              Paragraph that is not coherent.
-              These are not rigid rules to be followed but rather guidelines where you need to at least ponder on whether you paragraph is unjustly too long.
-
-              So what about coherence? Do not pack a bunch of content into one paragraph. For example, if you are listing a bunch 3–4 papers and their findings in a paragraph, it is acceptable to do so as long as you stay within e.g., 8 sentences. The further away that you are getting from this flexible target, the more likely it is that your paragraph is becoming too long. For example, in literature review a section make look like this:
-
-
-              This expansion typically is in part due to the fact that especially in literature review sections, people tend to want to mention things about the referenced papers (often more than what is required). If a single paper that you are citing is summarized in 4–5 sentences then chances are it requires its own paragraph. But then the paragraphs may look as if they are appearing out of the blue. How do you fix that? You can use an intro paragraph for your subsection like the image below.
-
-
-              It is a handy trick that can be used and highly effective for most sections of a research paper.
-
-              Detecting long paragraphs automatically
-              That got me wondering, is there an automated way to tell if your paragraphs are too long? Two applications that I am familiar with and can do this is Grammarly and Citasion although you need to use their premium version to get this feature (free versions have some features but not related to long paragraphs). Grammarly (starting $12/month) has no trial available for testing. Citasion (starting $2.99/month) has a three month trial for its premium features.
-
-              I decided to test each on 3 texts that are intentionally too long. To simplify things, I used paragraphs from Wikipedia articles that were contiguous and just merged them into a larger paragraph.
-
-              The first article was about earthquakes:
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non ullamcorper lacus, eget tincidunt nunc. Donec vitae dui vel nisl condimentum ultricies. Paragraphs are a concept that people are expected to learn during their high school years but people fail to grasp the importance of paragraph length. This is not an issue that I have seen on student papers but also on academic papers submitted to journals and conferences. So what is a long paragraph? It is a paragraph that feels a bit much but if I were to summarize it, here’s two identifiers: Paragraph consisting typically of more than 8 sentences. Paragraph that is not coherent. These are not rigid rules to be followed but rather guidelines where you need to at least ponder on whether you paragraph is unjustly too long. So what about coherence? Do not pack a bunch of content into one paragraph. For example, if you are listing a bunch 3–4 papers and their findings in a paragraph, it is acceptable to do so as long as you stay within e.g., 8 sentences. The further away that you are getting from this flexible target, the more likely it is that your paragraph is becoming too long. For example, in literature review a section make look like this: This expansion typically is in part due to the fact that especially in literature review sections, people tend to want to mention things about the referenced papers (often more than what is required). If a single paper that you are citing is summarized in 4–5 sentences then chances are it requires its own paragraph. But then the paragraphs may look as if they are appearing out of the blue. How do you fix that? You can use an intro paragraph for your subsection like the image below. It is a handy trick that can be used and highly effective for most sections of a research paper. Detecting long paragraphs automatically That got me wondering, is there an automated way to tell if your paragraphs are too long? Two applications that I am familiar with and can do this is Grammarly and Citasion although you need to use their premium version to get this feature (free versions have some features but not related to long paragraphs). Grammarly (starting $12/month) has no trial available for testing. Citasion (starting $2.99/month) has a three month trial for its premium features. I decided to test each on 3 texts that are intentionally too long. To simplify things, I used paragraphs from Wikipedia articles that were contiguous and just merged them into a larger paragraph. The first article was about earthquakes:
- 
-                        </p>
-          {/* Add more content as needed */}
+          {/* For small screens, move caption to the left and position the picture to the right */}
+          <div className="group flex sm:flex-row md:flex-col">
+            <div className="relative h-32 md:h-64 lg:h-96 w-full order-2 sm:order-1">
+              <img src="/image.png" alt="New in Accessories" className="w-full h-full object-cover" />
+            </div>
+            {/* Different caption for small screens */}
+            <div className="bg-white bg-opacity-80 p-2 flex items-center w-full order-1 sm:order-2">
+              <p className="text-sm md:text-xs text-black hidden sm:block">NEW IN RUGS AND ACCESSORIES →</p>
+              <p className="text-sm md:text-xs text-black sm:hidden">
+              <span className="block mb-1">Rugs &</span>
+                <span className="block mb-1">Accessories</span>
+                <span className="font-bold">NEW IN →</span>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-   
+    </div>
   );
 }
