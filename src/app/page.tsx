d7e965cfdab0be0a4b2030ed6fe33869354e9c60
Supratik Man Singh Pradhan <np01cp4a210087@islingtoncollege.edu.np>
2024-06-14 15:01:16 +0545

    heigh change of cards in lg screen Signed-off-by: Supratik Man Singh Pradhan <np01cp4a210087@islingtoncollege.edu.np>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className="bg-white min-h-screen">
       <div className="p-6 overflow-y-auto h-80vh">
-        <h1 className="text-1xl md:text-3xl font-bold text-black mb-4">Browse by Category</h1>
+        <h1 className="text-1xl md:text-2xl font-bold text-black mb-4">Browse by Category</h1>
         <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-4 md:gap-6 lg:gap-8">
           {/* Placeholder for the fourth item on larger screens */}
           <div className="hidden"></div>
 
           <div className="group flex sm:flex-row md:flex-col">
-            <div className="relative h-32 md:h-64 lg:h-96 w-full order-2 sm:order-1">
+            <div className="relative h-32 md:h-64 lg:h-[425px] w-full order-2 sm:order-1">
               <img src="/image.png" alt="New in Fabrics" className="w-full h-full object-cover" />
             </div>
             {/* Different caption for small screens */}
@@ -27,7 +27,7 @@ export default function Home() {
           <div className="hidden"></div>
 
           <div className="group flex sm:flex-row md:flex-col">
-            <div className="relative h-32 md:h-64 lg:h-96 w-full order-2 sm:order-1">
+            <div className="relative h-32 md:h-64 lg:h-[425px] w-full order-2 sm:order-1">
               <img src="/image.png" alt="New in Wallpapers" className="w-full h-full object-cover" />
             </div>
             {/* Different caption for small screens */}
@@ -43,7 +43,7 @@ export default function Home() {
           <div className="hidden"></div>
 
           <div className="group flex sm:flex-row md:flex-col">
-            <div className="relative h-32 md:h-64 lg:h-96 w-full order-2 sm:order-1">
+            <div className="relative h-32 md:h-64 lg:h-[425px] w-full order-2 sm:order-1">
               <img src="/image.png" alt="New in Trims" className="w-full h-full object-cover" />
             </div>
             {/* Different caption for small screens */}
@@ -61,7 +61,7 @@ export default function Home() {
 
           {/* For small screens, move caption to the left and position the picture to the right */}
           <div className="group flex sm:flex-row md:flex-col">
-            <div className="relative h-32 md:h-64 lg:h-96 w-full order-2 sm:order-1">
+            <div className="relative h-32 md:h-64 lg:h-[425px] w-full order-2 sm:order-1">
               <img src="/image.png" alt="New in Accessories" className="w-full h-full object-cover" />
             </div>
             {/* Different caption for small screens */}
